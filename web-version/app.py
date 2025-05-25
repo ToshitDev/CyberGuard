1d1e99ecfa44306aff5ae5525cef1382684ec5db
Sai Toshit Raj Repala <salaardevavratharaisaar@Sais-MacBook-Pro.local>
2025-05-25 06:36:45 +0530

    Cleaned up file structure for web version

--- web-version/app.py
+++ web-version/app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask import Flask, render_template, request
 from scanner import scan_target
 from utils import is_valid_target
